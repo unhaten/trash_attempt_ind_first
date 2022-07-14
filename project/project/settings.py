eb66380c85330f5c89c3b45ee5e46ec982432412
unhaten <csgold2002@mail.ru>
2022-07-14 17:17:03 +0300

    migrating to postgresql

--- project/project/settings.py
+++ project/project/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
     }
 }
 
